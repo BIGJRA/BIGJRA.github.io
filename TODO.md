c647fb9e947f09366be60e130f77a1b958afb636
BIGJRA <47573834+BIGJRA@users.noreply.github.com>
2023-08-26 22:24:50 -0400

    See if we can fix spoilers!

--- TODO.md
+++ TODO.md
@@ -1,1 +1,23 @@
 Add regular trainers... sometime in the future. Message me if you want to help with this.
+
+<style>
+  #button {color:red}
+</style>
+<div id="spoiler" style="display:none"> 
+HIDDEN CONTENT HERE
+</div> 
+<a display="initial" id="spoilerBtn" title="Click to show/hide content" type="button">Show hidden content</a>
+
+<div class="spoilerDiv">
+  <div class="spoilerText" style="display:none">
+    HIDDEN CONTENT HERE
+  </div>
+  <a display="initial" class="spoilerBtn" title="Click to show/hide content" type="button">Show hidden content</a>
+</div>
+
+<div class="spoilerDiv">
+  <div class="spoilerText" style="display:none">
+    HIDDEN CONTENT  2
+  </div>
+  <a display="initial" class="spoilerBtn" title="Click to show/hide content" type="button">Show hidden content 2</a>
+</div>
--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,7 @@
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -17,8 +17,13 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-$(document).ready(function(){
-  $("img.tabImage").each(function() {
-     $(this).attr("onclick", "window.open(this.src, '_blank');")
+$(document).ready(function () {
+  $("img.tabImage").each(function () {
+    $(this).attr("onclick", "window.open(this.src, '_blank');")
   });
 });
+
+$('.spoilerBtn').click(function() {
+	$(this).parent().find('.spoilerText').css('display', 'block');
+  $(this).hide();
+})
