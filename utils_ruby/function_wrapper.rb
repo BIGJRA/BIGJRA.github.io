04a82e5c3c947d036681a459e24acde5b7284ba4
BIGJRA <47573834+BIGJRA@users.noreply.github.com>
2024-05-13 16:47:53 -0400

    Wild held

--- utils_ruby/function_wrapper.rb
+++ utils_ruby/function_wrapper.rb
@@ -37,7 +37,8 @@ class FunctionWrapper
       "enc" => "generate_encounter_markdown",
       "shop" => "generate_shop_markdown",
       "battle" => "generate_trainer_markdown",
-      "mine" => "generate_mining_markdown"
+      "mine" => "generate_mining_markdown",
+      "wildheld" => "generate_wild_held_markdown"
     }
 
   end
@@ -107,6 +108,87 @@ class FunctionWrapper
     return html_output.split("\n")[1..].join("\n")
   end
 
+  def generate_wild_held_markdown()
+    # Create the main hash with default value as a proc
+    lookup_hash = Hash.new { |hash, key| hash[key] = { "common" => [], "uncommon" => [], "rare" => [] } }
+  
+    @pokemonHash.each do |mon_symbol, form_hash|
+      f = form_hash.reject! { |key| !key.is_a?(String) }.compact
+      next if not f
+  
+      f.each do |form, data|
+        lookup_hash[data[:WildItemCommon]]["common"] << [mon_symbol, form] if data[:WildItemCommon]
+        lookup_hash[data[:WildItemUncommon]]["uncommon"] << [mon_symbol, form] if data[:WildItemUncommon]
+        lookup_hash[data[:WildItemRare]]["rare"] << [mon_symbol, form] if data[:WildItemRare]
+      end
+    end
+  
+    # Creates nokogiri HTML
+    doc = Nokogiri::HTML::Document.new
+    div = doc.create_element('div', class: 'mining_table')
+    doc.add_child(div)
+
+    table = doc.create_element('table')
+    div.add_child(table)
+
+    # Creates the header for the table
+    thead = doc.create_element('thead')
+    table.add_child(thead)
+    
+    table_header = doc.create_element('th', colspan: 2)
+    thead.add_child(table_header)
+
+    bold = doc.create_element('strong')
+    bold.content = "Wild Pokemon Held Item Chances"
+    table_header.add_child(bold)
+    table_header['class'] = 'table-header'
+    table_header['style'] = 'text-align: center;'
+    
+    # Sorts items by order in item hash
+    lookup_hash.map {|item, mon_hash| [item, mon_hash]}.sort_by {|a, _| @itemHash.keys.index(a)}.each do |item, mon_hash| 
+
+      result = ''
+      next if !mon_hash
+      mon_hash.each do |rarity, pokemon_list|
+        next if pokemon_list.empty?
+    
+        # Transform each Pokémon entry into the desired format
+        pokemon_string = pokemon_list.map do |pokemon, form|
+          form_1_key = @pokemonHash[pokemon].keys.find_all { |key| key.is_a?(String) }[0]
+          form_1_data = @pokemonHash[pokemon][form_1_key]
+          pokemon_name = "#{@pokemonHash[pokemon][form_1_key][:name]}"
+          if form == "Alolan Form"
+            "#{pokemon_name} (#{form})"
+          else
+            pokemon_name.to_s
+          end
+        end.join(", ")
+    
+        # Concatenate the rarity and Pokémon string
+        result << "- #{rarity.capitalize} (#{{"common" => 50, "uncommon" => 5, "rare" => 1}[rarity]}%): #{pokemon_string}\n"
+      end
+      result = result.chomp
+
+      content_row = doc.create_element('tr')
+      table.add_child(content_row)
+      
+      # Column 1: Item Name (italicized)
+      td_item = doc.create_element('td', style: 'text-align: center')
+      td_item.add_child(doc.create_element('em', content=@itemHash[item][:name]))
+      content_row.add_child(td_item)
+    
+      # Column 2: Mon List With Prob
+      td_price = doc.create_element('td')
+      td_price.content = "#{result}"
+      content_row.add_child(td_price)
+    end
+
+    html_output = doc.to_html 
+    return html_output.split("\n")[1..].join("\n")
+    
+  end
+  
+
   def generate_encounter_markdown(map_id, enc_type_exclude_list = nil)
     return @encGetter.get_encounter_md(map_id, enc_type_exclude_list)
   end
@@ -124,6 +206,7 @@ end
 
 def main
   fw = FunctionWrapper.new('reborn')
+  puts fw.generate_wild_held_markdown
 end
 
 main if __FILE__ == $PROGRAM_NAME
