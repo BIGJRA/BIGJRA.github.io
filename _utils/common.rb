bc9d61daca65e1c877021f2bde6031294d876577
BIGJRA <47573834+BIGJRA@users.noreply.github.com>
2026-08-04 00:15:31 -0700

    version

--- _utils/common.rb
+++ _utils/common.rb
@@ -7,7 +7,7 @@ UTILS_DIR = File.dirname(File.expand_path(__FILE__))
 ROOT_DIR = File.dirname(UTILS_DIR)
 CONFIG = YAML.safe_load(File.open(File.join(ROOT_DIR, '_config.yml')))
 
-VERSIONS = { 'reborn' => "19.5.18" , 'rejuv' => "13.5.6", 'deso' => "6.0.13" }
+VERSIONS = { 'reborn' => "19.5.18" , 'rejuv' => "14.0.15", 'deso' => "6.0.13" }
 
 LONGNAMES = { 'reborn' => 'reborn', 'rejuv' => 'rejuvenation', 'deso' => 'desolation'}
 
